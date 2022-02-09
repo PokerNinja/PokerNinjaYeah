e5bc9a2408fd10b64bd51ee9feb7538b0645757c
PokerNinja <ninjaoabk@gmail.com>
2022-02-09 17:41:58 +0200

    o

--- Assets/Scripts/GameCore/PuDeckUi.cs
+++ Assets/Scripts/GameCore/PuDeckUi.cs
@@ -106,9 +106,18 @@ public class PuDeckUi : MonoBehaviour, IPointerDownHandler
 
 
 
-    public void CANCELWAHT()
+    public void CANCELWAHT(bool enable)
     {
-        player
+        if (enable)
+        {
+
+            playerPusUi[0].spriteRenderer.material.EnableKeyword("_FadeOn");
+        }
+        else
+        {
+
+            playerPusUi[0].spriteRenderer.material.DisableKeyword("_FadeOn");
+        }
     }
 
     private int ConvertCardPlaceToIndex(string puPlace)
